c01082fabcba18657c30ce8cc55f20ab9ec4a45b
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-26 13:53:51 -0500

    start answering question 3

--- advanced javascript logic.js
+++ advanced javascript logic.js
@@ -92,5 +92,100 @@ let arrayOrganizer = function(array){
     return console.log(`Pairs that equal ${total} `), addedPair;
   }
  
+  // Question 3: Write a function that converts HEX to RGB. Then Make that function auto-detect the 
+  // formats so that if you enter HEX color format it returns RGB and if you enter RGB color format it returns HEX.
+
+// What do the numbers and letters in a hex code mean?
+// Hex color codes start with a pound sign or hashtag (#) and are followed by six letters and/or numbers. 
+// The first two letters/numbers refer to red, the next two refer to green, and the last two refer to blue. 
+// The color values are defined in values between 00 and FF (instead of from 0 to 255 in RGB).
+
+// Numbers are used when the value is 1-9. Letters are used when the value is higher than 9. For example:
+
+// A=10
+
+// B=11
+
+// C=12
+
+// D=13
+
+// E=14
+
+// F=15
+
+// To get a hexadecimal color, follow these three steps:
+
+// Multiply the first number by 16.
+
+// Multiply the second number by 1.
+
+// Add the two totals together. 
+
+
+// 1) create makeArray function that converts data into an array
+// 2) create a function that detects RGB vs HEX
+// 3) declare hexHead variable equal to '#'
+// 4) declare rgbHead variable equal to 'RGB('
+// 5) declare rgbTail variable equal to ')'
+// 6) declare stringHolder variable equal to an empty array
+// 7) create findPath function that takes in stringHolder variable and asks a conditional to determine
+//          if we're going to convert from hex to rgb or rgb to hex
+// 7a)
+// 2) if RGB convert to Hex
+// 3) if Hex convert to RGB
+//    call makeArray function with data as an argument
+//    b. 
+// 4) create a function that converts a string into an array 
+
+
+
+
+
+function colorConverter (data){
+  let hexHead = '#';
+  let rgbHead = 'RGB(';
+  let rgbTail = ')';
+  let stringHolder = [];
+
+  function makeArray (){
+    for (let index = 0; index < data.length; index++) {
+      let slicedLetter = data.slice(index, index+1);
+      console.log(slicedLetter);
+      stringHolder.push(slicedLetter);
+      console.log(stringHolder);
+    }
+    return stringHolder
+  }
+
+  function findPath (stringHolder){
+    (stringHolder[0] === '#') ? makeRGB(stringHolder) : makeHex(stringHolder);
+  }
+
+  function makeRGB(data){
+    makeArray(data);
+
+  };
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
